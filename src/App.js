b755552e3138130d3b52a28f3525590558de1159
vishvas patel <110728766+vishvas1410@users.noreply.github.com>
2024-06-14 13:17:09 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Appointment from "./Pages/Appointment";
 function App() {
   return (
     <div className="App">
-      <Router basename="/Health-Plus">
+      <Router basename="/">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/legal" element={<Legal />} />
